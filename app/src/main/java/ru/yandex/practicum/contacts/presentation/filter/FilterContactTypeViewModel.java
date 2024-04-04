f723a137c17fb919f526e1d2605db452854dd872
Vitaliy Yarash <vit.iarosch@yandex.by>
2024-04-04 22:51:51 +0300

    feat: resolve 2 task

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/FilterContactTypeViewModel.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/FilterContactTypeViewModel.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.contacts.presentation.filter;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
@@ -60,13 +61,15 @@ public class FilterContactTypeViewModel extends BaseBottomSheetViewModel {
     }
 
     private void updateFilterContactTypes() {
-        // создайте массив строк. Для инициализации используйте метод ContactType.getContactTypes(),
-        // который должен возвращать список всех доступных источников контактов
+        String[] contactTypeList = ContactType.getContactTypes();
 
-        // создайте список типа FilterContactTypeUi и заполните его с помощью цикла forEach
-        // forEach должен бежать по строковому массиву, который вы создали ранее
-
+        ArrayList<FilterContactTypeUi> filterContactsList = new ArrayList<>();
+        filterContactsList.add(createAllSelectedItem(contactTypeList));
+        for (String contact: contactTypeList) {
+            filterContactsList.add(createFilterContactType(contact));
+        }
         // вызовите меотод setValue() у переменной filterContactTypesLiveDate и передайте в качестве аргументы ваш список типа FilterContactTypeUi
+        filterContactTypesLiveDate.setValue(filterContactsList);
     }
 
     @NonNull
